bd28a1dcdc66eb39fb6c501d51e56df7b0fadd9c
Oana Tanasoiu <oana.tanasoiu@puppet.com>
2020-02-07 11:33:27 +0200

    (PA-3114) Add acceptance tests

--- acceptance/tests/facter_ng_command_works.rb
+++ acceptance/tests/facter_ng_command_works.rb
@@ -6,13 +6,14 @@ test_name 'PA-3114: Use facter-ng on all platform' do
   require 'puppet/acceptance/common_utils'
 
   agents.each do |agent|
-    step "test facter-ng command'"
-    facter_ng_dir = String.new
-    on agent, "#{gem_command(agent)} which facter-ng'", :acceptable_exit_codes => [0] do
-      facter_ng_dir = stdout.chomp
-    end
-    facter_ng_dir = facter_ng_dir.match(/\.*facter-ng-[0-9]+\.[0-9]+\.[0-9]+/).to_s
+    step "test facter-ng command'" do
+      facter_ng_dir = String.new
+      on agent, "#{gem_command(agent)} which facter-ng'", :acceptable_exit_codes => [0] do
+        facter_ng_dir = stdout.chomp
+      end
+      facter_ng_dir = facter_ng_dir.match(/\.*facter-ng-[0-9]+\.[0-9]+\.[0-9]+/).to_s
 
-    on agent, "#{facter_ng_dir}/bin/facter-ng", :acceptable_exit_codes => [0]
+      on agent, "#{facter_ng_dir}/bin/facter-ng", :acceptable_exit_codes => [0]
+    end
   end
 end
